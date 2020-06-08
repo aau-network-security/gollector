83ed4c65a30b7ceec59bae9bc50ee3bc240fb95a
gianmarcomennecozzi <gianmarco.mennecozzi@gmail.com>
2020-06-08 15:59:31 +0200

    created just for benchmark purpose

--- app/ct/main.go
+++ app/ct/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"sync"
+	"time"
+
 	"github.com/aau-network-security/gollector/api"
 	prt "github.com/aau-network-security/gollector/api/proto"
 	"github.com/aau-network-security/gollector/collectors/ct"
@@ -14,8 +17,6 @@ import (
 	"github.com/vbauerster/mpb/v4"
 	"github.com/vbauerster/mpb/v4/decor"
 	"google.golang.org/grpc/metadata"
-	"sync"
-	"time"
 )
 
 var (
@@ -117,6 +118,8 @@ func main() {
 	m := sync.Mutex{}
 	progress := 0
 
+	startTime := time.Now()
+
 	for _, l := range logs {
 		go func(l ct.Log) {
 			var count int64
@@ -186,9 +189,9 @@ func main() {
 
 			opts := ct.Options{
 				WorkerCount: conf.WorkerCount,
-				StartIndex:  start,
+				StartIndex:  0,
 				//EndIndex:    end,
-				EndIndex: start + 100,
+				EndIndex: 10000,
 			}
 
 			count, err = ct.Scan(ctx, &l, entryFn, opts)
@@ -198,7 +201,7 @@ func main() {
 		}(l)
 	}
 	p.Wait()
-
+	fmt.Println(time.Since(startTime))
 	if err := bs.CloseSend(ctx); err != nil {
 		log.Fatal().Msgf("error while closing connection to server: %s", err)
 	}
