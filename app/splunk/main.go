50e1d86baa7ff42f38968f40f3fb4ae17f890083
kdhageman <kdhageman@users.noreply.github.com>
2021-03-15 13:22:33 +0100

    Fix log level

--- app/splunk/main.go
+++ app/splunk/main.go
@@ -119,6 +119,12 @@ func main() {
 		log.Fatal().Msgf("error while reading configuration: %s", err)
 	}
 
+	logLevel, err := zerolog.ParseLevel(conf.LogLevel)
+	if err != nil {
+		log.Fatal().Msgf("error while parsing log level: %s", err)
+	}
+	zerolog.SetGlobalLevel(logLevel)
+
 	cc, err := conf.ApiAddr.Dial()
 	if err != nil {
 		log.Fatal().Msgf("failed to dial: %s", err)
